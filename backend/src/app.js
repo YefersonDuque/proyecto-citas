a04d0ffbc997adb1f72af003d273f26def0c396b
YefersonDuque1012 <auxsisinfo2@hospitalrionegro.gov.co>
2026-08-14 15:58:23 -0500

    app.listen

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,7 @@
-console.log("Backend funcionando");
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Servidor ejecutándose en http://localhost:3000");
+});
