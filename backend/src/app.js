defb834fa4c982dcfb4aa2d190fcbe9d6699201b
YefersonDuque1012 <auxsisinfo2@hospitalrionegro.gov.co>
2026-08-19 11:05:28 -0500

    feat: implementar consulta de citas en mobile

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 
 const profesionalesRoutes = require("./routes/profesionales.routes");
 const usuariosRoutes = require("./routes/usuarios.routes");
 const citasRoutes = require("./routes/citas.routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(profesionalesRoutes);
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,98 +1,188 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
+import { StyleSheet } from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+type Cita = {
+  oid: number;
+  paciente: string;
+  profesional: string;
+  especialidad: string;
+  motivo: string;
+  fecha_cita: string;
+  hora_cita: string;
+  estado_cita: string;
+};
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+function capitalizarTexto(texto: string) {
+  return texto
+    .toLowerCase()
+    .split(" ")
+    .map((palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1))
+    .join(" ");
+}
+
+function capitalizarPrimeraLetra(texto: string) {
+  return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
+}
+
+function formatearFecha(fecha: string) {
+  const fechaSinHora = fecha.split("T")[0];
+  const partes = fechaSinHora.split("-");
+  return `${partes[2]}/${partes[1]}/${partes[0]}`;
+}
+
+function formatearHora(hora: string) {
+  const partes = hora.split(":");
+  return `${partes[0]}:${partes[1]}`;
 }
 
 export default function HomeScreen() {
+  const [documento, setDocumento] = useState("");
+  const [citas, setCitas] = useState<Cita[]>([]);
+  const [mensaje, setMensaje] = useState("");
+
+  const buscarCitas = async () => {
+    setMensaje("");
+    setCitas([]);
+
+    if (!documento) {
+      setMensaje("Por favor, ingresa tu documento");
+      return;
+    }
+    const respuesta = await fetch(
+      `http://localhost:3000/usuarios/${documento}/citas`,
+    );
+
+    const datos = await respuesta.json();
+
+    if (!respuesta.ok) {
+      setMensaje(datos.message);
+      setCitas([]);
+      return;
+    }
+
+    if (!Array.isArray(datos)) {
+      setMensaje(datos.message);
+      setCitas([]);
+      return;
+    }
+    setMensaje("");
+    setCitas(datos);
+  };
+
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Citas Médicas</Text>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+      <View style={styles.form}>
+        <Text style={styles.label}>Documento</Text>
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
+        <TextInput
+          style={styles.input}
+          value={documento}
+          onChangeText={setDocumento}
+        />
+        <Pressable style={styles.button} onPress={buscarCitas}>
+          <Text style={styles.buttonText}>Buscar citas</Text>
+        </Pressable>
+      </View>
 
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+      {mensaje !== "" && <Text>{mensaje}</Text>}
+      {citas.map((cita) => (
+        <View style={styles.card} key={cita.oid}>
+          <View style={styles.row}>
+            <Text style={styles.fieldLabel}>Profesional</Text>
+            <Text>{capitalizarTexto(cita.profesional)}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.fieldLabel}>Especialidad</Text>
+            <Text>{capitalizarPrimeraLetra(cita.especialidad)}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.fieldLabel}>Motivo</Text>
+            <Text>{capitalizarPrimeraLetra(cita.motivo)}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.fieldLabel}>Fecha</Text>
+            <Text>{formatearFecha(cita.fecha_cita)}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.fieldLabel}>Hora</Text>
+            <Text>{formatearHora(cita.hora_cita)}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.fieldLabel}>Estado</Text>
+            <Text>{capitalizarPrimeraLetra(cita.estado_cita)}</Text>
+          </View>
+        </View>
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    padding: 20,
   },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
+
+  form: {
+    width: "100%",
+    maxWidth: 500,
+    alignSelf: "center",
   },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
+
   title: {
-    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 30,
   },
-  code: {
-    textTransform: 'uppercase',
+
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 15,
+  },
+
+  button: {
+    width: "100%",
+    backgroundColor: "#2563EB",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  card: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 15,
+    marginTop: 20,
+    width: "100%",
+    maxWidth: 500,
+    alignSelf: "center",
+  },
+
+  fieldLabel: {
+    fontWeight: "bold",
+    width: 100,
+  },
+
+  row: {
+    flexDirection: "row",
   },
 });
